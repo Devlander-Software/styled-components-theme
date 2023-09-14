68bb995087594f185f051bf1949553f7b24756ca
Landon Johnson <landonwjohnson@gmail.com>
2023-09-14 17:36:33 -0600

    v1.0.56

--- src/defaults/all-icon-names.ts
+++
@@ -1,8 +0,0 @@
-import { FontAwesomeIconNames, IconNames, IoniconNames, MaterialIconNames } from "../types/icon-names.enum";
-
-const AllMaterialIconNames = Object.keys(MaterialIconNames);
-const AllIoniconNames = Object.values(IoniconNames);
-const AllFontAwesomeIconNames = Object.values(FontAwesomeIconNames);
- const AllIconNames = [...AllMaterialIconNames, ...AllIoniconNames, ...AllFontAwesomeIconNames];
-
- export default AllIconNames;
