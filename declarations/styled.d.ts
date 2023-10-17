dd354932d2d9dfdc814a5f010773fb2a22981611
Landon Johnson <landonwjohnson@gmail.com>
2023-10-16 20:15:30 -0600

    updated build

--- declarations/styled.d.ts
+++ declarations/styled.d.ts
@@ -1,11 +1,11 @@
 import "styled-components";
 import "styled-components/native";
 import { ThemeInterface, ThemeInterfaceForHTML } from "../src/types/theme.types";
-export declare module "styled-components/native" {
+ declare module "styled-components/native" {
     export interface DefaultTheme extends ThemeInterface {}
 }
   
 
- export declare module "styled-components" {
+  declare module "styled-components" {
   export interface DefaultTheme extends ThemeInterfaceForHTML {}
 }
